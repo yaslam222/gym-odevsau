b69dc1758440c11036150d1ae7d00994b9de0462
fuadaishan <fu.abdullah20@gmail.com>
2025-12-13 22:53:32 +0300

    admin classes and contracts

---
+++ SporSalonuProjesi/Migrations/20251101235131_fix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SporSalonuProjesi.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "GymClasses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "GymClasses");
+        }
+    }
+}
