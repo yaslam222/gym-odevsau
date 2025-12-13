ed6cc6229811d7ec6e35aad1156f9268113fc8e4
fuadaishan <fu.abdullah20@gmail.com>
2025-12-13 22:52:23 +0300

    add admin vip

---
+++ SporSalonuProjesi/Areas/Admin/Controllers/AdminViptrialController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SporSalonuProjesi.Data;
+
+namespace SporSalonuProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminViptrialController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AdminViptrialController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var viptrials = dbContext.VipRequests.ToList();
+            return View(viptrials);
+        }
+        public IActionResult Delete(int id)
+        {
+            var viptrial = dbContext.VipRequests.Find(id);
+            if (viptrial != null)
+            {
+                dbContext.VipRequests.Remove(viptrial);
+                dbContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
